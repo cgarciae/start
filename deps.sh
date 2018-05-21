44113c7755bfcfb30cd81dbc940bcdb106c48531
Cristian Garcia <cgarcia.e88@gmail.com>
2018-05-21 10:39:47 -0500

    init

---
+++ deps.sh
@@ -0,0 +1,19 @@
+# update
+sudo apt-get update
+
+# deps
+sudo apt-get install -y \
+    git \
+    zsh \
+    tmux \
+    keychain
+
+# oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# spaceship theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
---
+++ files.sh
@@ -0,0 +1,9 @@
+
+# make dir
+mkdir /tmp/cristian
+
+# clone
+git clone https://github.com/cgarciae/start /tmp/cristian/start
+
+# copy files
+cp "/tmp/cristian/start/home-files/*" ~
