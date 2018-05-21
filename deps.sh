7fdc9af58b4a07539f411646a4c5632af5b64474
Cristian Garcia <cgarcia.e88@gmail.com>
2018-05-21 11:41:27 -0500

    gcloud

--- deps.sh
+++ deps.sh
@@ -9,7 +9,11 @@ sudo apt-get install -y \
     keychain \
     curl
 
-
+# install gcloud
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk
 
 # install anaconda
 CUR_DIR=$(pwd)
