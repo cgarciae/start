fc02cd1b78396daa3fdfec80f09e9c103a6aaa81
Cristian Garcia <cgarcia.e88@gmail.com>
2018-05-22 16:09:17 -0500

    docker-compose

--- deps.sh
+++ deps.sh
@@ -19,6 +19,10 @@ sudo apt-get update && sudo apt-get install -y google-cloud-sdk
 #install docker
 bash -c "$(wget https://get.docker.com/ -O -)"
 
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # go to downloads
 CUR_DIR=$(pwd)
 cd ~/Downloads
