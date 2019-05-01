693df40d529a7325ffa93518de319dd60727407f
Cristian Garcia <cristian@pop-os.localdomain>
2019-05-01 15:53:51 -0500

    fish

--- deps.sh
+++ deps.sh
@@ -4,13 +4,17 @@ sudo apt-get update
 # deps
 sudo apt-get install -y \
     git \
-    zsh \
+    fish \
     tmux \
     keychain \
     curl \
     htop \
     rsync \
     inotify-tools
+
+# install fisher + pure
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fisher add rafaelrinaldi/pure
 
 # install gcloud
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
@@ -22,33 +26,28 @@ sudo apt-get update && sudo apt-get install -y google-cloud-sdk
 bash -c "$(wget https://get.docker.com/ -O -)"
 
 # docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# nvidia-docker
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install nvidia-docker2
 
 # go to downloads
 CUR_DIR=$(pwd)
 cd ~/Downloads
 
 # install anaconda
-curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash Anaconda3-5.0.1-Linux-x86_64.sh
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
 
 # back to folder
 cd $CUR_DIR
 
-#temporal exports
-export ZSH="/home/cristian/.oh-my-zsh"
-export ZSH_CUSTOM="$ZSH/custom"
-
-# oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-# spaceship theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-# auto suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # change shell
- chsh -s /usr/bin/tmux
+chsh -s /usr/bin/tmux
