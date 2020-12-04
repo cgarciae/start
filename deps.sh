da7ceddf8a0da8983475a928e856ef115e14e42d
Carlos Andrés Álvarez Restrepo <charlie_cha@outlook.com>
2020-12-03 20:56:58 -0500

    Update deps.sh
    Add nvidia docker

--- deps.sh
+++ deps.sh
@@ -60,6 +60,14 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
+# install nvidia docker
+distribution=ubuntu20.04
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+
 # docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
