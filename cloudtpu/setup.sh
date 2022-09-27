9297dbab7efc70be2fb131e4da8d1e050422b09c
Cristian Garcia <cgarcia.e88@gmail.com>
2022-09-26 19:09:57 -0500

    Update setup.sh

--- cloudtpu/setup.sh
+++ cloudtpu/setup.sh
@@ -9,9 +9,6 @@ sudo apt-get install -y make cmake python-is-python3
 pip install poetry
 ~/.local/bin/poetry config virtualenvs.in-project true
 
-# create ssh key
-ssh-keygen -t ed25519 -C "cgarcia.e88@gmail.com" -f ~/.ssh/id_ed25519 -q -N ""
-
 # fish
 sudo apt install -y fish
 fish -c 'curl -sL https://git.io/fisher | source'
@@ -21,3 +18,7 @@ fish -c 'fisher install pure-fish/pure'
 # git
 git config --global user.name "Cristian Garcia"
 git config --global user.email "cgarcia.e88@gmail.com"
+
+# create ssh key
+ssh-keygen -t ed25519 -C "cgarcia.e88@gmail.com" -f ~/.ssh/id_ed25519 -q -N ""
+cat ~/.ssh/id_ed25519.pub
