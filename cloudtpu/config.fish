a3d8ffbb73c11063bf100c51a34b20d0824aff36
Cristian Garcia <cgarcia.e88@gmail.com>
2022-09-28 15:05:24 -0500

    Update config.fish

--- cloudtpu/config.fish
+++ cloudtpu/config.fish
@@ -6,11 +6,7 @@ set -x PATH ~/.local/bin $PATH
 #----------------------------------------------------------------------
 # pyenv
 #----------------------------------------------------------------------
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH $PYENV_ROOT/bin $PATH
-status --is-interactive; and pyenv init --path | source
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
+pyenv init - | source
 
 function dpython
     echo "Waiting for debugger..."
