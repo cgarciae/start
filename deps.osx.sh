a2f25b37c2e95a808b20ef703b6f37951b5d8d54
Cristian Garcia <cgarcia.e88@gmail.com>
2023-01-19 12:54:35 -0500

    Create deps.osx.sh

---
+++ deps.osx.sh
@@ -0,0 +1,15 @@
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> ~/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# install fish
+brew install fish
+fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
+fish -c 'fisher install pure-fish/pure'
+
+# gcloud
+brew install --cask google-cloud-sdk
+
+#
